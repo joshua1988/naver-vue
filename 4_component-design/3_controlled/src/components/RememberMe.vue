712d04cc25b9fb495157e6038e55ea1091fc4bba
joshua1988 <jangkeehyo@gmail.com>
2019-05-31 14:15:53 +0900

    change v-model customizing code

--- 4_component-design/3_controlled/src/components/RememberMe.vue
+++ 4_component-design/3_controlled/src/components/RememberMe.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <label for="me">remember me</label>
-    <input id="me" type="checkbox" v-on:click="checkRemember" v-bind:value="value">
-  </div>  
+    <input id="me" type="checkbox" v-on:click="checkRemember" v-bind:checked="checked">
+  </div>
 </template>
 
 <script>
 export default {
-	props: ['value'],
+	model: {
+		prop: 'checked',
+		event: 'change',
+	},
+	props: {
+		checked: Boolean,
+	},
 	methods: {
 		checkRemember: function() {
-			this.$emit('input', !this.value);
+			this.$emit('change', !this.checked);
 		},
 	},
 };
